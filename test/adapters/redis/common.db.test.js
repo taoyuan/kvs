00116c73e4993f67d4190f4433e59d102ff44b0c
taoyuan <torworx@gmail.com>
2015-07-20 22:27:59 +0800

    fixed not call ready if no `db` options provided

--- test/adapters/redis/common.db.test.js
+++ test/adapters/redis/common.db.test.js
@@ -1,9 +1,9 @@
 "use strict";
 
-describe('Redis adapter common features', function() {
+describe('Redis adapter common features with `db` option', function() {
 
     function getStore(cb) {
-        var store = require('../../../').Store('redis');
+        var store = require('../../../').Store('redis', {db: 2});
         store.ready(function () {
             cb(store);
         });
