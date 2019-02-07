2474764f605060d0bea2874cb6448dc52d311114
ty <towyuan@outlook.com>
2019-02-07 11:34:25 +0800

    Fix store function to return Store instance directly

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import {Store, StoreOptions} from './store';
+import { Store, StoreOptions } from "./store";
 import { AdapterInitializer } from "./adapter";
 
-export * from './store';
+export * from "./store";
 export * from "./adapter";
 export * from "./bucket";
 
-export async function store(name: string, settings?: StoreOptions): Promise<Store>;
-export async function store(settings: StoreOptions): Promise<Store>;
-export async function store(initializer: AdapterInitializer): Promise<Store>;
-export async function store(name: string | StoreOptions | AdapterInitializer, settings?: StoreOptions): Promise<Store> {
-  const store = new Store(name, settings);
-  await store.ready();
-  return store;
+export function store(name: string, settings?: StoreOptions): Store;
+export function store(settings: StoreOptions): Store;
+export function store(initializer: AdapterInitializer): Store;
+export function store(name: string | StoreOptions | AdapterInitializer, settings?: StoreOptions): Store {
+  return new Store(name, settings);
 }
