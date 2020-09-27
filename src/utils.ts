7dbea9e2cbdb5829e5c53daebc50ed41d4bd4391
ty <towyuan@outlook.com>
2020-09-27 16:21:29 +0800

    chore: re-implement plain ts format function

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,34 @@
-import util from 'util';
+export function format(fmt: string, ...args: any[]): string {
+  const re = /(%?)(%([jds]))/g;
+  if (args.length) {
+    fmt = fmt.replace(re, function (match, escaped, ptn, flag) {
+      let arg: any = args.shift();
+      switch (flag) {
+        case 's':
+          arg = '' + arg;
+          break;
+        case 'd':
+          arg = Number(arg);
+          break;
+        case 'j':
+          arg = JSON.stringify(arg);
+          break;
+      }
+      if (!escaped) {
+        return arg;
+      }
+      args.unshift(arg);
+      return match;
+    });
+  }
 
-export function format(fmt: any, ...params: any[]): string {
-  return util.format(fmt, ...params);
-}
+  // arguments remain after formatting
+  if (args.length) {
+    fmt += ' ' + args.join(' ');
+  }
 
-export async function asyncFromCallback<T>(
-  fn: (cb: (err: any, data: T) => any) => any,
-): Promise<T> {
-  return new Promise<T>((resolve, reject) => {
-    fn((err, data) => (err ? reject(err) : resolve(data)));
-  });
+  // update escaped %% values
+  fmt = fmt.replace(/%{2}/g, '%');
+
+  return fmt;
 }
